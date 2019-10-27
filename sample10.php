f9e6a80e78ecd995738d3ece22306e78bc7f8a19
ishiharayusuke <wkjrc300@yahoo.co.jp>
2019-10-27 17:14:38 +0900

    if文

---
+++ sample10.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+
+<?php
+//if(date('G') < 16){
+//print('※現在受け付け時間外です');
+//}else{
+//print('ようこそ');
+//}
+
+$x = '';
+if($x !== '' ){
+print('xには文字がはいっています');
+}
+?>
+</pre>
+</main>
+</body>    
+</html>  
